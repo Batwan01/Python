85b555e0e299334d159ccedbfaeba30d59b4d415
Batwan01 <wldhks1120@naver.com>
2023-12-29 17:55:18 +0900

    experience

--- hello.py
+++ hello.py
@@ -1,1 +1,33 @@
-print("hello")
+#hello my name is {}\n".format("jiwan")
+
+#a="jiwan"
+#b="korean"
+#"i'm {1} and {0}".format(a,b)
+
+#"{0:>10}".format("hi")
+#"{0:<10}".format("hi")
+#"{0:^10}".format("hi")
+
+'''names='jiwan'
+age=23
+f'나의 이름은 {names}이고, 나이는 {age}입니다.''''
+
+a="hobby"
+a.count('b')
+a.find('y') #존재하지 않으면 -1 반환
+
+",".join("abcd") #a,b,c,d
+
+a="hi"
+a.upper() #"HI"
+a.lower() #"hi"
+
+a="  hi    "
+a.lstrip() #왼쪽 공백 제거
+a.rstrip() #오른쪽 공백 제거
+
+a = "Life is too short"
+a.split()
+
+b = "a:b:c:d"
+b.split(':')
