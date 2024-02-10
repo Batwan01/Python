30e57d6133aabfeefd9ce1b9f1a0e0303530c2c3
Batwan01 <wldhks1120@naver.com>
2024-02-10 12:39:48 +0900

    Track ML

--- ML.py
+++ ML.py
@@ -1,4 +1,16 @@
 def reward_function(params):
 
   track_width = params['track_width']
-  distance_from_center = params
+  distance_from_center = params['distance_from_center']
+
+  market_1 = 0.1 * track_width
+  market_2 = 0.5 * track_width
+
+  if distance_from_center <= market_1:
+    reward = 2.0 # Close to center line
+  elif distance_from_center <= market_2:
+    reward = 0.2 # Futher from center line
+  else:
+    reward = 1e-3 # Off track
+
+  return float(reward)
