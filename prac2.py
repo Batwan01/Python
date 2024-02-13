20ebb4dfc019dab0d14c7a1cec7d65d380a9e2c1
Batwan01 <wldhks1120@naver.com>
2024-02-13 18:49:16 +0900

    2day

--- prac2.py
+++ prac2.py
@@ -13,3 +13,18 @@ t2*3 #(3,4,3,4,3,4)
 len(t1) #3
 
 t1+t2
+
+a = {1: 'hi'} #key = 1 , value = 'hi'
+a = {'a': [1,2,3]} #value에 리스트 삽입 가능
+a['name'] = 'jiwan' #name(Key) = jiwan(Value)
+del a['name'] #name 키 삭제
+a.keys()
+
+s1=set("Hello")
+s1
+
+s1 = set([1,2,3,4,5,6])
+s2 = set([4,5,6,7,8,9])
+s1 & s2
+s1 | s2
+s2 - s1
