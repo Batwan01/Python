038a109aaf8b457a757a016b7d6d19dc241a2329
Batwan01 <wldhks1120@naver.com>
2024-03-13 21:02:17 +0900

    hmm

---
+++ baekjoon/M1316.py
@@ -0,0 +1,5 @@
+N = int(input())
+count = 0
+for i in range(N):
+  word = list(input())
+  print(word)
---
+++ baekjoon/M1436.py
@@ -0,0 +1,21 @@
+count = 3 #6의 개수
+check = 0 #앞 뒤 증가 위치 정하기
+
+N = int(input())
+if N<14:
+  pass
+else:
+  count = N / 13 #6의 개수
+  time = N % 13 #반복횟수
+
+if time<8:
+  pass
+else:
+  time-7
+  check = 1
+
+if check == 0:
+  
+    
+    
+
