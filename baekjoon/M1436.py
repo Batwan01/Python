2ef9a1da1fb2bb29d0da544ceccd62fa09166364
Batwan01 <128815650+Batwan01@users.noreply.github.com>
2024-03-18 17:41:55 +0900

    24. 3. 18.

--- baekjoon/M1436.py
+++ baekjoon/M1436.py
@@ -1,4 +1,15 @@
 N = int(input())
-
-for i in range(10000):
-    
+count = 0
+i = 0
+while True:
+    i+=1
+    str_i = str(i)
+    len_i = len(str_i)
+    for j in range(len_i):
+        if len_i>j+2 and str_i[j] == '6':
+            if str_i[j+1]=='6' and str_i[j+2]=='6':
+                count+=1
+                break
+    if count == N:
+        print(i)
+        break
