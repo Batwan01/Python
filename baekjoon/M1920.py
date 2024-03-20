faf657457fc533b6993a3e0a16e30023e1a9df74
Batwan01 <128815650+Batwan01@users.noreply.github.com>
2024-03-20 13:31:33 +0900

    24. 3. 20.

--- baekjoon/M1920.py
+++ baekjoon/M1920.py
@@ -1,51 +1,11 @@
 ans_N = int(input())
-answer = list(map(int, input().split()))
-answer.sort()
+answer = set(map(int, input().split()))
 
 com_N = int(input())
 compar = list(map(int, input().split()))
-check = sorted(compar)
 
-count = 0
-for num in answer:
-    while count < com_N and check[count] < num:
-        count+=1
-    if check[count] == num:
-        check[count] = -1
-        count+=1
-
-last = 0
-for num in compar:
-    last = 0
-    for i in check:
-        if num == i:
-            last+=1
-            continue
-    if last == 0:
+for i in compar:
+    if i in answer:
         print('1')
     else:
         print('0')
-
-
-
-# samenums = []
-# for i in answer:
-#     for j in check:
-#         if i == j:
-#             samenums.append(i)
-#         elif i<j:
-#             break
-
-# for i in samenums:
-#     for j in range(len(compar)):
-#         if i == compar[j]:
-#             compar[j] = -1
-
-# for i in range(len(compar)):
-#     if compar[i] == -1:
-#         compar[i] = 1
-#     else:
-#         compar[i] = 0
-
-# for i in compar:
-#     print(i)
