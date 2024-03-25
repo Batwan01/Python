52d0e47ccaa1b850ebaef0f567691bd1ea377fad
Batwan01 <128815650+Batwan01@users.noreply.github.com>
2024-03-25 16:51:57 +0900

    24. 3. 25.

--- baekjoon/M11866.py
+++ baekjoon/M11866.py
@@ -1,14 +1,16 @@
 N, K = map(int, input().split())
 
 array = []
-for i in range(1,N+1):
+for i in range(1, N+1):
     array.append(i)
 
+idx = 0
+print('<', end='')
 for i in range(N):
-    print((K-1)%len(array)+1)
-# for i in range(N):
-#     print(array)
-#     del array[K-1]
-#     K+=3
-#     if K-1>len(array):
-#         K = abs(K-N)
+    idx = (idx + K -1) % len(array)
+    print(array.pop(idx), end = '')
+    if len(array) == 0:
+        break
+    else:
+        print(end = ', ')
+print('>')
