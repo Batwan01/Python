f9a7b472239f62bf53eafac821103bfffd3cf64e
Batwan01 <128815650+Batwan01@users.noreply.github.com>
2024-03-28 09:51:47 +0900

    24. 3. 28.

---
+++ baekjoon/M10845.py
@@ -0,0 +1,36 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+
+que = []
+instr = []
+for i in range(N):
+    instr = list(map(str, input().split()))
+
+    if instr[0] == 'push':
+        que.append(instr[1])
+    elif instr[0] == 'pop':
+        if len(que) == 0:
+            print('-1') 
+        else:
+            print(que.pop(0))
+    elif instr[0] == 'size':
+        if len(que) == 0:
+            print('-1')
+        else:
+            print(len(que))
+    elif instr[0] == 'empty':
+        if len(que) == 0:
+            print('1') 
+        else:
+            print('0')
+    elif instr[0] == 'front':
+        if len(que) == 0:
+            print('-1') 
+        else:
+            print(que[0])
+    elif instr[0] == 'back':
+        if len(que) == 0:
+            print('-1') 
+        else:
+            print(que[-1])
