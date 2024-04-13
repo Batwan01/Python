66508b91f22a1881f6de80e4032fa04937243b18
Batwan01 <128815650+Batwan01@users.noreply.github.com>
2024-04-13 20:11:23 +0900

    24. 4. 13.

--- baekjoon/M2839.py
+++ baekjoon/M2839.py
@@ -1,17 +1,30 @@
 N = int(input())
 
 num=0
-if N%5>2:
-    num = N//5
-    if (N-(num*5))%3 != 0:
-        if N%3==0:
-            print(N//3)
-        else:
-            print(-1)
-    else:
-        num = num + (N-(num*5))//3
-        print(num)
-elif N%3==0:
-    print(N//3)
-else:
-    print(-1)
+
+for i in range(5,0,-1):
+     print(i)
+
+
+
+
+
+
+
+
+
+
+# if N%5>2:
+#     num = N//5
+#     if (N-(num*5))%3 != 0:
+#         if N%3==0:
+#             print(N//3)
+#         else:
+#             print(-1)
+#     else:
+#         num = num + (N-(num*5))//3
+#         print(num)
+# elif N%3==0:
+#     print(N//3)
+# else:
+#     print(-1)
