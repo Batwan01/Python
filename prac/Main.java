d4736e702606c1b3beaf6355b072e8a4af8da633
Batwan01 <128815650+Batwan01@users.noreply.github.com>
2024-04-22 02:04:17 +0900

    .

---
+++ prac/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = read();
+        System.out.println(n);
+    }
+
+    public static int read() {
+        Scanner s = new Scanner(System.in);
+        return s.nextInt();
+    }
+}
