d605108ba063698feb4e9c7a7f41b6cfa2c1ba8d
Batwan01 <128815650+Batwan01@users.noreply.github.com>
2024-04-30 14:02:41 +0900

    .

--- baekjoon/M2839.py
+++ baekjoon/M2839.py
@@ -1,30 +1,25 @@
 N = int(input())
+count = 0
+num = 0
+ori = N
+flag = False
+if N >= 5:
+     count = N//5
+     N = N % 5
+     if N == 0:
+          print(count)
+          flag = True
 
-num=0
-
-for i in range(5,0,-1):
-     print(i)
-
-
-
-
-
-
-
-
-
-
-# if N%5>2:
-#     num = N//5
-#     if (N-(num*5))%3 != 0:
-#         if N%3==0:
-#             print(N//3)
-#         else:
-#             print(-1)
-#     else:
-#         num = num + (N-(num*5))//3
-#         print(num)
-# elif N%3==0:
-#     print(N//3)
-# else:
-#     print(-1)
+if N%3==0 and flag == False:
+     count += N//3
+     print(count)
+elif ori>=6 and flag == False:
+     count-=1
+     N+=5
+     if N%3 == 0:
+          count+=N//3
+          print(count)
+     else:
+          print(-1)
+elif flag == False:
+     print(-1)
