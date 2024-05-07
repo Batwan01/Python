0ba8dc0e9186c509328f42fdd6b1f8bc15463413
박지완 <wldhks1120@naver.com>
2024-05-07 09:35:34 +0900

    24. 5. 7.

--- baekjoon/M2839.py
+++ baekjoon/M2839.py
@@ -3,23 +3,28 @@ count = 0
 num = 0
 ori = N
 flag = False
+answer = 0
 if N >= 5:
      count = N//5
      N = N % 5
      if N == 0:
           print(count)
           flag = True
-
 if N%3==0 and flag == False:
      count += N//3
      print(count)
 elif ori>=6 and flag == False:
-     count-=1
-     N+=5
-     if N%3 == 0:
-          count+=N//3
-          print(count)
-     else:
+     for i in range(1,count):
+          count-=1  
+          N+=5
+          if N%3 == 0:
+               count+=N//3
+               print(count)
+               flag = True
+               break
+     if ori%3 == 0 and flag == False:
+          print(ori//3)
+     elif flag == False:
           print(-1)
 elif flag == False:
      print(-1)
