d3b4ee4dbba36aeaef482de2fd9fc6b30d1b19c0
박지완 <wldhks1120@naver.com>
2024-05-09 13:03:06 +0900

    24. 5. 9.

--- baekjoon/M4949.py
+++ baekjoon/M4949.py
@@ -1,8 +1,50 @@
-stack = []
+word = []
+count = 0
+flag = True
 
 while True:
-    stack.append(input())
-    if stack[0] == '.':
+    word.append(input())
+    count += 1
+    if word[0] == '.':
         break
-    elif stack[-1][-1] == '.':
-        word
+    elif word[-1][-1] == '.':
+        while True:
+            stack = []
+            if len(word) == 0:
+                break
+            words = word.pop(0)
+            count -= 1
+            for i in range(len(words)):
+                if words[i] == '(' or words[i] == '[' or words[i] == '{':
+                    stack.append(words[i])
+                elif words[i] == ')' and len(stack) != 0:
+                    if stack.pop() == '(':
+                        continue
+                    else:
+                        print('no')
+                        flag = False
+                        break
+                elif words[i] == '] and len(stack) != 0':
+                    if stack.pop() == '[':
+                        continue
+                    else:
+                        print('no')
+                        flag = False
+                        break
+                elif words[i] == '}' and len(stack) != 0:
+                    if stack.pop() == '{':
+                        continue
+                    else:
+                        print('no')
+                        flag = False
+                        break
+                else:
+                    flag = False
+                    print('no')
+                    break
+            if flag == True:
+                print('yes')
+            else:
+                flag = True
+    else:
+        continue
