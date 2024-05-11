997d0b6058f8cdf958d83175ab32215baf7715d1
박지완 <wldhks1120@naver.com>
2024-05-11 15:47:32 +0900

    .

--- baekjoon/M2108.py
+++ baekjoon/M2108.py
@@ -1,16 +1,31 @@
 import sys
 input = sys.stdin.readline
 
+MAX = 0
+NUM = 0
 arr = []
 count = []
+flag = False
 N = int(input())
 for i in range(N):
     arr.append(int(input()))
 
-count = set(arr)
-print(arr, count)
 arr.sort()
-print(sum(arr) // N)
-print(arr[N//2])
+count = set(arr)
+print(int(round(sum(arr) / N,0)))
 
+print(arr[N//2])
+for num in count:
+    if arr.count(num)>MAX:
+        MAX = arr.count(num)
+        NUM = num
+for num in count:
+    if MAX == arr.count(num) and flag == False:
+        flag = True
+    elif MAX == arr.count(num) and flag == True:
+        flag = 2
+        print(num)
+        break
+if flag != 2:
+    print(NUM)
 print(abs(max(arr)-min(arr)))
