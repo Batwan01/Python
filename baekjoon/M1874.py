3d9e4a638a0b8c00bab96e1b970f3f0c83f5721c
박지완 <wldhks1120@naver.com>
2024-05-13 19:18:56 +0900

    .

--- baekjoon/M1874.py
+++ baekjoon/M1874.py
@@ -1,10 +1,18 @@
 stack = []
 answer = []
-
+input_num = []
+flag = False
 N = int(input())
 
-for i in range(1,N+1):
-    answer.append(i)
-    stack.append(int(input()))
+for i in range(N):
+    input_num.append(int(input()))
 
-print(answer,stack)
+count = 0
+while N < count:
+    for i in range(1,N+1):
+        if i < input_num[count]:
+            answer.append(i)
+            count+=1
+        elif i == input_num[count]:
+            answer.append(i)
+            count+=1
