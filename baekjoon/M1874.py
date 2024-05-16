e3fa65565882c26a9f4a70c3ca13dc0a8101d848
박지완 <wldhks1120@naver.com>
2024-05-16 10:08:41 +0900

    24. 5. 16.

--- baekjoon/M1874.py
+++ baekjoon/M1874.py
@@ -1,10 +1,37 @@
+import sys
+input = sys.stdin.readline
+
 stack = []
 answer = []
-
+input_num = []
+flag = False
 N = int(input())
 
+for i in range(N):
+    input_num.append(int(input()))
+    
+count = 0
 for i in range(1,N+1):
-    answer.append(i)
-    stack.append(int(input()))
+    stack.append(i)
 
-print(answer,stack)
+    if stack[-1] == input_num[count]:
+        stack.pop()
+        answer.append('+')
+        answer.append('-')
+        # print("+")
+        # print("-")
+        count+=1
+    else:
+        # print("+")
+        answer.append('+')
+    while len(stack)!=0 and stack[-1] == input_num[count]:
+        stack.pop()
+        answer.append('-')
+        # print("-")
+        count+=1
+        
+if len(stack)!=0:
+    print("NO")
+else:
+    for num in answer:
+        print(num)
