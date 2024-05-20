b24ea024c32fb3f26dc920e1bc55f1b126929261
박지완 <wldhks1120@naver.com>
2024-05-20 14:47:24 +0900

    .

--- programmers/lv1_2.py
+++
@@ -1,7 +0,0 @@
-#9시 39분 ->
-#s를 숫자로 변환한 결과
-def solution(s):
-    return int(s)
-
-solution("1234")
-solution("-1234")
--- programmers/lv1_4.py
+++
@@ -1,6 +0,0 @@
-#11시 4분 -> 8분
-#N의 각 자릿수의 합을 구해서 return 하는 solution 함수
-#N = 123이면 1 + 2 + 3 = 6
-
-def solution(n):
-    return sum([int(num) for num in list(str(n))])
