ce336be70d461b24071bbee93e68d3b5a48c5d3f
박지완 <128815650+Batwan01@users.noreply.github.com>
2024-06-07 14:30:18 +0900

    [level 1] Title: 추억 점수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -1,14 +1,11 @@
 
 def solution(name, yearning, photo):
     answer = []
-    dict = {}
-    for ch,num in zip(name,yearning):
-        dict[ch] = num
-    
+    dic = dict(zip(name,yearning))
     for photo_list in photo:
         sum = 0
         for photo_name in photo_list:
-            if photo_name in dict:
-                sum += dict[photo_name]
+            if photo_name in dic:
+                sum += dic[photo_name]
         answer.append(sum)
     return answer
