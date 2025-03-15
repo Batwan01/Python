39ac61db73c54c09556fd04508899f268f69aef0
박지완 <128815650+Batwan01@users.noreply.github.com>
2025-03-15 20:45:21 +0900

    [level 1] Title: K번째수, Time: 0.02 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,9 +1,9 @@
-# i번째 숫자부터 j번째 숫자까지 자르고 정렬했을 때 k 번째 값 구하기
-# [1, 2, 3, 4, 5] -> 2,5,3 -> [2,3,4,5] 정렬 -> 4
 def solution(array, commands):
     answer = []
-    arr = []
-    for ar in commands:
-        arr = sorted(array[ar[0]-1:ar[1]])
-        answer.append(arr[ar[2]-1])
+    
+    for command in commands:
+        arr = array[command[0]-1:command[1]]
+        arr = sorted(arr)
+        print(arr)
+        answer.append(arr[command[2]-1])
     return answer
