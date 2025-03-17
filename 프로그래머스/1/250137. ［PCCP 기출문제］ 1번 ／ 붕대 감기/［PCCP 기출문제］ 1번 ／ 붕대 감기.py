2168ba6ef3e6199f1b4da28f58620ecea4cf542c
박지완 <128815650+Batwan01@users.noreply.github.com>
2025-03-17 18:09:44 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -1,29 +1,23 @@
 def solution(bandage, health, attacks):
-    answer = health  # 현재 체력
+    answer = health # 현재 체력
 
     for i in range(len(attacks) - 1):
-        # 공격 받기
-        answer -= attacks[i][1]
-        if answer <= 0: 
-            return -1  # 체력이 0 이하가 되면 즉시 종료
-
+        
+        answer-=attacks[i][1]
+        if answer <= 0: return -1
+    
         attack_gap = attacks[i + 1][0] - attacks[i][0]
-        if attack_gap <= 1: 
-            continue  # 연속 성공 시간이 1초 이하이면 회복할 필요 없음
+        if attack_gap <= 1: continue
+        elif attack_gap > bandage[0]:
+            if attack_gap//bandage[0] >= 1:
+                answer+= (attack_gap-1)*bandage[1] + bandage[2] * (attack_gap//bandage[0])
+            else:
+                answer+= (attack_gap-1)*bandage[1] + bandage[2]
         else:
-            full_cycles = (attack_gap - 1) // bandage[0]  # 연속 성공 횟수
-            remaining_time = (attack_gap - 1) % bandage[0]  # 남은 시간
+            answer+= (attack_gap-1)*bandage[1]
             
-            # 기본 회복량 + 연속 성공 시 추가 회복
-            answer += (attack_gap - 1) * bandage[1] + full_cycles * bandage[2]
+        if answer > health: answer = health
 
-        # 최대 체력 초과 방지
-        if answer > health: 
-            answer = health
-
-    # 마지막 공격 처리
-    answer -= attacks[-1][1]
-    if answer <= 0: 
-        return -1
-
+    answer-=attacks[-1][1]
+    if answer <=0 : return -1
     return answer
